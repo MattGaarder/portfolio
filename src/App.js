81daaf7215360c79c153dd179927b975c86fcd7b
MattGaarder <matteus.gaarder@gmail.com>
2023-05-27 15:10:42 +0100

    implements react router and starts to create new pages for about and thoughts sections

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import React from "react";
 // import NavBar from "./components/NavBar/NavBar";
 import {ProjectCard, MainContent, NavBar, CoolCounter, ProgressBar} from './components'
 import projects from "./projects.json";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Thoughts from "./pages/Thoughts";
+import Something from "./pages/Something";
 
 function App() {
   return (
     <>
       <NavBar />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/thoughts" element={<Thoughts />} /> 
+          <Route path="/something" element={<Something />} />
+        </Routes>
       <MainContent />
       <div className="grid-container">
         {projects.map((p) => (
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 function NavBar() {
   const [scrolled, setScrolled] = useState(false);
@@ -28,34 +29,48 @@ function NavBar() {
   if (scrolled) {
     navbarClasses.push('scrolled');
   }
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // Smooth scroll
+  });
+};
+
     return (
       <>
         <header>
           <nav className={`nav-nav ${scrolled ? "scrolled" : ""}`}>
-            <p className="name">Matt Gaarder</p>
+            <p className="name" onClick={scrollToTop}>Matt Gaarder</p>
             <ul className="nav-items">
-              <p>About</p>
-              <p>Thoughts</p>
-              <p>Projects</p>
+              <CustomLink to="/about">About</CustomLink>
+              <CustomLink to="/thoughts">Thoughts</CustomLink>
+              <CustomLink to="/something">Something</CustomLink>
             </ul>
             <div className="link-icons">
               <a href="https://github.com/MattGaarder" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="nav-icon" icon={faGithub} /></a>
               <a href="https://www.linkedin.com/in/matteus-gaarder-991494178/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="nav-icon" icon={faLinkedin} /></a>
               <a href="https://twitter.com/JelloAudible" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="nav-icon" icon={faTwitter} /></a>
-              <FontAwesomeIcon className="nav-icon pdf" icon={faFilePdf} />
+              <a href="/GAARDER-CV.pdf" download>
+                <FontAwesomeIcon className="nav-icon pdf" icon={faFilePdf} />
+              </a>
             </div>
           </nav>
-          {/* <nav className="nav-2">
-            <div className="icons nav-items-2">
-            <a href="https://github.com/MattGaarder" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="icon" icon={faGithub} /></a>
-            <a href="https://www.linkedin.com/in/matteus-gaarder-991494178/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="icon" icon={faLinkedin} /></a>
-            <a href="https://twitter.com/JelloAudible" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="icon" icon={faTwitter} /></a>
-            <FontAwesomeIcon className="icon" icon={faFilePdf} />
-            </div>
-          </nav> */}
         </header>
       </>
     );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
 }
 
 export default NavBar
