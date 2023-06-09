c7ad76237adef5e216a7a71e8d858207424d0da1
MattGaarder <matteus.gaarder@gmail.com>
2023-06-09 12:52:48 +0200

    tries to mess around with margins so that about elements line up with navbar elements

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,17 +3,17 @@ import "./About.css"
 function About({ navbarPositions}) {
     return (
     <div className="about-grid-container">
-        <div className="about-container" id="content1" style={{marginLeft: navbarPositions.section1?.left, marginRight: navbarPositions.section1?.right}}>
-            <div className="grid-area-1">1</div>
+        <div className="about-container1" id="content1" style={{marginLeft: navbarPositions.section1?.left, marginRight: navbarPositions.section1?.right}}>
+            1
         </div>
-        <div className="about-container" id="content2" style={{marginLeft: navbarPositions.section2?.left, marginRight: navbarPositions.section2?.right}}>
-            <div className="grid-area-2">2</div>
+        <div className="about-container2" id="content2" style={{marginLeft: navbarPositions.section2?.left, marginRight: navbarPositions.section2?.right}}>
+            2
         </div>
-        <div className="about-container" id="content3" style={{marginLeft: navbarPositions.section3?.left, marginRight: navbarPositions.section3?.right}}>
-            <div className="grid-area-3">3</div>
+        <div className="about-container3" id="content3" style={{marginLeft: navbarPositions.section3?.left, marginRight: navbarPositions.section3?.right}}>
+            3
         </div>
-        <div className="about-container" id="content4" style={{marginLeft: navbarPositions.section4?.left, marginRight: navbarPositions.section4?.right}}>
-            <div className="grid-area-4">4</div>
+        <div className="about-container4" id="content4" style={{marginLeft: navbarPositions.section4?.left, marginRight: navbarPositions.section4?.right}}>
+            4
         </div>
     </div>
     )
