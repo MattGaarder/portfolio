a85b1602c8d39a0449ff235fb52a5bbfed8511ef
MattGaarder <matteus.gaarder@gmail.com>
2023-06-11 15:23:32 +0200

    improves grid placement

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,30 +1,46 @@
-import "./About.css"
+import "./About.css";
 
+function About({ navbarPositions }) {
+  console.log(navbarPositions);
+  let headerWidth = 0;
+  if (
+    navbarPositions.section4 &&
+    navbarPositions.section2 &&
+    navbarPositions.section3
+  ) {
+    headerWidth =
+      navbarPositions.section2.width +
+      navbarPositions.section3.width +
+      navbarPositions.section4.width;
+  }
+  console.log("this is the width: " + headerWidth);
+  const section1Width = navbarPositions.section1?.width || 0;
 
-
-
-
-function About({ navbarPositions}) {
-    console.log(navbarPositions)
-    let headerWidth = 0;
-    if(navbarPositions.section4 && navbarPositions.section2 && navbarPositions.section3){
-      headerWidth = navbarPositions.section2.width + navbarPositions.section3.width + navbarPositions.section4.width;
-    }
-    console.log("this is the width: " + headerWidth)
-    const section1Width = navbarPositions.section1?.width || 0;
-    const gridTemplateColumns = `${section1Width}px ${headerWidth}px`;
-    return (
-    <div className="about-grid-container" style={{gridTemplateColumns}}>
-        <div className="about-container grid-area" style={{width: `${section1Width}px`}}>About
+  return (
+    <div
+      className="about-grid-container"
+      style={{ gridTemplateColumns: `${section1Width}px ${headerWidth}px` }}
+    >
+      <div className="row1 grid-area">
+        <div
+          className="about-container"
+          style={{ width: `${section1Width}px` }}
+        >
+          About
         </div>
-        <div className="header-container grid-area" style={{width: `${headerWidth}px`}}>Header
+        <div className="header-container" style={{ width: `${headerWidth}px` }}>
+          Header
         </div>
-        <div className="main-container grid-area" style={{width: `50px`}}>Main
-        </div>
-        <div className="section-container grid-area" style={{width: `100px`}}>Section
-        </div>
+      </div>
+      <div
+        className="row2 grid-area"
+        style={{ gridTemplateColumns: "2fr 1fr" }}
+      >
+        <div className="main-container">Main</div>
+        <div className="section-container">Section</div>
+      </div>
     </div>
-    )
+  );
 }
 
 export default About;
